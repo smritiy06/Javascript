781e80ff28f9cb4c8875db0bbe5ff6196ad3a0aa
Smriti Yadav <smritiy1234@gmail.com>
2026-06-15 18:01:59 +0530

    learned const, let and var

---
+++ 01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1002   //const -> constant
+let accountEmail = "smriti@gmail.com"        //declairing variables
+var accountPassword = "1234"              //prefer not to use var in js
+accountCity = "Jamshedpur"
+
+accountEmail = "smriti12334@gmail.com"        //declairing variables
+accountPassword = "321321"
+accountCity = "Jaipur"
+// accountId = 1003   not allowed
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity])
