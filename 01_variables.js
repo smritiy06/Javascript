661dd7284f7ded61d1508a012475a23df0074d13
Smriti Yadav <smritiy1234@gmail.com>
2026-06-15 18:03:05 +0530

    learned const, let and var

--- 01_variables.js
+++ 01_variables.js
@@ -2,10 +2,11 @@ const accountId = 1002   //const -> constant
 let accountEmail = "smriti@gmail.com"        //declairing variables
 var accountPassword = "1234"              //prefer not to use var in js
 accountCity = "Jamshedpur"
+let accState
 
 accountEmail = "smriti12334@gmail.com"        //declairing variables
 accountPassword = "321321"
 accountCity = "Jaipur"
 // accountId = 1003   not allowed
 console.log(accountId)
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity, accState])
