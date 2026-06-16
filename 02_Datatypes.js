634ee0ecc39dbaad38d8fc8d91ec88f153908fcd
Smriti Yadav <smritiy1234@gmail.com>
2026-06-16 15:44:47 +0530

    Datatypes

---
+++ 02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict"      // treat all JS code as newer version
+
+// alert("Hello")      // we are using nodejs, not browser
+
+console.log(2+9)
+console.log("Hello")
+let name = "Smriti"     // string datatype
+let age = 19     // number datatype
+let isLoggedIn = true    // boolean datatype
+let state = null     // no value assigned
+
+
+// number -> 2^53
+// bigInt
+// string -> "" or ''
+// boolean -> true/false
+// null -> standalone value
+// undefined -> value not assigned
+// symbol -> unique
+
+// object 
+
+console.log(typeof undefined)  // undefined
+console.log(typeof null)  // object
