af1365133945069e67d7b7ed205ec1d90aac9926
Smriti Yadav <smritiy1234@gmail.com>
2026-06-16 16:03:37 +0530

    Convertion

---
+++ 03_ConvertionOperation.js
@@ -0,0 +1,32 @@
+let score = true
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);  
+
+
+/* "33" -> 33
+    "33ab" -> NaN
+    true -> 1,  false -> 0
+*/
+
+let isLoggedIn = "smriti"
+
+let BooleanLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanLoggedIn);
+
+
+/* 1 -> true
+    0 -> false
+    "smriti" -> true
+    "" -> false
+*/
+
+
+let a = 33
+let stringA = String(a)
+console.log(stringA);
+console.log(typeof stringA);
